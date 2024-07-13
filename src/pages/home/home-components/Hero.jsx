02378af20810d9da643bd78a36c6294b42dbf48b
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-07-13 19:19:11 +0530

    Ehnaced hero text with animation

--- src/pages/home/home-components/Hero.jsx
+++ src/pages/home/home-components/Hero.jsx
@@ -1,8 +1,17 @@
 function Hero() {
   return (
-    <section className="py-32 flex justify-center items-center rounded-xl shadow-sm shadow-gray-900  border-white hero">
+    <section className="py-8 flex items-center justify-center rounded-xl shadow-sm shadow-gray-900  border-white hero text-center">
       <div>
-        <h1 className="text-white">Find your dream job</h1>
+        <h1 className="text-white animate-slidein [--slidein-delay:400ms] opacity-0">
+          Find your dream job with{" "}
+          <span className="animate-slidein [--slidein-delay:500ms] opacity-0 text-underlay-1">
+            HirelyAi
+          </span>
+        </h1>
+        <p className="text-slate-400 animate-slidein [--slidein-delay:600ms] opacity-0 text-sm">
+          Explore endless opportunities and take the next step towards your
+          professional growth. Your future starts here!
+        </p>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   prefix: "",
   theme: {
@@ -66,12 +66,17 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        slidein: {
+          from: { opacity: "0", transform: "translateY(-10px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
