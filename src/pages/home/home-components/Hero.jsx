8c17f5da7a48692d7b2587c7af7097068961bbdb
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-07-15 03:28:35 +0530

    Implemented Hero section with cta

--- src/pages/home/home-components/Hero.jsx
+++ src/pages/home/home-components/Hero.jsx
@@ -1,18 +1,63 @@
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { Link } from "react-scroll";
+
 function Hero() {
+  const { isLoaded, isSignedIn, user } = useUser();
+
   return (
     // <section className="py-8 flex items-center justify-center rounded-xl shadow-sm shadow-gray-900  border-white hero text-center">
-    <section className="py-16 flex items-center justify-center text-center">
+    <section className="flex items-center justify-center text-center h-screen">
       <div>
         <h1 className="animate-slidein [--slidein-delay:400ms] opacity-0">
-          Find your dream job with{" "}
+          Find your dream job with {""}
           <span className="animate-slidein [--slidein-delay:500ms] opacity-0 text-underlay-1">
             HirelyAi
           </span>
         </h1>
-        <p className="animate-slidein [--slidein-delay:600ms] opacity-0 text-base">
+        <p className="animate-slidein [--slidein-delay:600ms] opacity-0 text-base text-slate-500 dark:text-slate-400">
           Explore endless opportunities and take the next step towards your
           professional growth. Your future starts here!
         </p>
+        <p className="animate-slidein [--slidein-delay:700ms] opacity-0 text-base text-slate-500 dark:text-slate-400 ">
+          Innovative AI platform that transforms the way you find and hire top
+          talent.
+        </p>
+        {!isSignedIn ? (
+          <div className="flex flex-col gap-y-4 items-center my-20">
+            <Button
+              asChild
+              className="w-fit py-7 px-5 animate-slidein [--slidein-delay:800ms] opacity-0"
+            >
+              <Link to={"/sign-up"}>
+                Explore your oppotunity
+                <span className="ml-2 text-sm">&rarr;</span>
+              </Link>
+            </Button>
+
+            <Link
+              to={"/sign-in"}
+              className="hover:underline hover:text-themecolor-lightblue animate-slidein [--slidein-delay:900ms] opacity-0"
+            >
+              Sign In
+            </Link>
+          </div>
+        ) : (
+          <div className="flex flex-col my-24 gap-6 items-center text-slate-500 dark:text-slate-200 animate-slidein [--slidein-delay:900ms] opacity-0">
+            <span className="text-2xl">
+              Hello! {user.firstName}, {""}
+            </span>
+            <Link
+              to="jobs"
+              smooth={true}
+              duration={500}
+              offset={-112}
+              className="text-3xl hover:underline text-themecolor-lightblue dark:text-themecolor-lightblue animate-slidein [--slidein-delay:900ms] opacity-0 text-slate-500 dark:text-slate-400 cursor-pointer"
+            >
+              Find out latest jobs <span className="ml-2 text-2xl">&darr;</span>
+            </Link>
+          </div>
+        )}
       </div>
     </section>
   );
