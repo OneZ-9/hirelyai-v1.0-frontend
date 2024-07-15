f86278da663d1b95bd629fc79be6891cdb58fbc1
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-07-16 03:46:29 +0530

    Added search field into JobSection and Admin JobPostSection

--- src/feature-components/admin/JobPostsSection.jsx
+++ src/feature-components/admin/JobPostsSection.jsx
@@ -1,9 +1,13 @@
 // import useFetchJobs from "../../hooks/useFetchJobs";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getJobs } from "@/lib/services/api/jobs";
 
 import Spinner from "@/components/shared/Spinner";
 import JobCard from "../job/JobCard";
+import SearchField from "@/components/shared/SearchField";
+import ErrorComponent from "@/components/shared/ErrorComponent";
+import Message from "@/components/shared/Message";
 
 function JobPostsSection() {
   // const { jobs, isLoading } = useFetchJobs();
@@ -12,18 +16,48 @@ function JobPostsSection() {
     data: jobs,
     error,
   } = useQuery({ queryKey: ["jobs"], queryFn: getJobs });
-  // console.log(jobs);
+  const [filteredJobs, setFilteredJobs] = useState([]);
+
+  function filterJobs(searchQuery) {
+    const filter = jobs?.filter(
+      (job) =>
+        job.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        job.company.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        job.type.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        job.location.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    return filter;
+  }
 
   if (isLoading) return <Spinner />;
+  if (error) return <ErrorComponent />;
 
   return (
     <section className="py-8">
-      <h2>Current Job Postings</h2>
+      <div className="flex items-center justify-between py-6">
+        <h2>Current Job Postings</h2>
+
+        <SearchField
+          className="min-w-[420px]"
+          placeholder="Search jobs by job title, company, type or location"
+          disabled={isLoading}
+          filterFunction={filterJobs}
+          setFilter={setFilteredJobs}
+        />
+      </div>
       <div className="mt-4 flex flex-col gap-y-4">
         {isLoading ? (
           <Spinner />
         ) : (
-          jobs.map((job) => <JobCard key={job._id} job={job} isAdmin={true} />)
+          <div className="mt-4 flex flex-col gap-y-8">
+            {filteredJobs.length ? (
+              filteredJobs?.map((job) => (
+                <JobCard key={job._id} job={job} isAdmin={true} />
+              ))
+            ) : (
+              <Message message="There is no matching results with search..." />
+            )}
+          </div>
         )}
       </div>
     </section>
