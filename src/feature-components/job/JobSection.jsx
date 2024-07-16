b673312a5291e90fac88123f88d82c88d2ddf53e
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-07-16 18:40:15 +0530

    Added search field to JobSection

--- src/feature-components/job/JobSection.jsx
+++ src/feature-components/job/JobSection.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import { useUser } from "@clerk/clerk-react";
 import { useQuery } from "@tanstack/react-query";
 import { getJobs } from "@/lib/services/api/jobs";
@@ -5,30 +6,64 @@ import { getJobs } from "@/lib/services/api/jobs";
 import JobCard from "../job/JobCard";
 import Spinner from "@/components/shared/Spinner";
 import ErrorComponent from "@/components/shared/ErrorComponent";
+import Message from "@/components/shared/Message";
+import SearchField from "@/components/shared/SearchField";
 
 function JobSection() {
-  const { user } = useUser();
+  const { user, isLoaded } = useUser();
   const isAdmin = user?.publicMetadata?.role === "admin";
-
+  const [filteredJobs, setFilteredJobs] = useState([]);
   const {
     isLoading,
     data: jobs,
     error,
   } = useQuery({ queryKey: ["jobs"], queryFn: getJobs });
 
+  // const handleSetFilteredJobs = useCallback((filtered) => {
+  //   setFilteredJobs(filtered);
+  // }, []);
+
+  const filterJobs = useCallback(
+    (searchQuery) => {
+      const filter = jobs?.filter(
+        (job) =>
+          job.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          job.company.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          job.type.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          job.location.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      return filter;
+    },
+    [jobs]
+  );
+
   if (error) return <ErrorComponent />;
 
   return (
     <section id="jobs" className="py-8 container">
-      <h2 className="py-6">Available Jobs</h2>
+      <div className="flex items-center justify-between py-6">
+        <h2>Available Jobs</h2>
 
-      {isLoading ? (
+        <SearchField
+          className="min-w-[420px]"
+          placeholder="Search jobs by job title, company, type or location"
+          disabled={isLoading}
+          filterFunction={filterJobs}
+          setFilter={setFilteredJobs}
+        />
+      </div>
+
+      {isLoading || !isLoaded ? (
         <Spinner />
       ) : (
         <div className="mt-4 flex flex-col gap-y-8">
-          {jobs.map((job) => (
-            <JobCard job={job} key={job._id} isAdmin={isAdmin} />
-          ))}
+          {filteredJobs.length ? (
+            filteredJobs?.map((job) => (
+              <JobCard job={job} key={job._id} isAdmin={isAdmin} />
+            ))
+          ) : (
+            <Message message="There is no matching results with search..." />
+          )}
         </div>
       )}
     </section>
