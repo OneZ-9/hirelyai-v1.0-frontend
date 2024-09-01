51af09eedee660ad19d3fc5e5991bde75b6b1fbb
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-09-01 17:12:43 +0530

    Changed text-align

--- src/components/shared/Message.jsx
+++ src/components/shared/Message.jsx
@@ -1,6 +1,6 @@
 function Message({ message }) {
   return (
-    <div className="h-80 flex items-center justify-center p-40 text-slate-500 dark:text-slate-400">
+    <div className="h-80 flex items-center justify-center text-center md:p-40 text-slate-500 dark:text-slate-400">
       <p>{message}</p>
     </div>
   );
