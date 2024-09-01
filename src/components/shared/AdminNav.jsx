4f1376385632b3d8ca241a6d724da4e0a2c15e5d
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-09-01 23:20:15 +0530

    Resposive AdminNav

--- src/components/shared/AdminNav.jsx
+++ src/components/shared/AdminNav.jsx
@@ -5,13 +5,17 @@ import Logo from "./Logo";
 
 function AdminNav() {
   return (
-    <nav className="sticky top-0 bg-background z-50 flex py-10 justify-between items-center">
+    <nav className="sticky top-0 bg-background z-50 h-[50px] sm:h-[125px] flex justify-between items-center py-8 px-1 sm:py-10 sm:px-16 shadow-sm dark:border-b-2 dark:border-b-slate-900">
       <Logo />
 
-      <div className="flex justify-end items-center gap-x-6 py-4">
-        <Link to="/admin/jobs">Job Posts</Link>
-        <Button asChild>
-          <Link to="/admin/job/create">Post A Job</Link>
+      <div className="flex justify-center gap-x-2 sm:gap-x-8 items-center">
+        <Link to="/admin/jobs" className="max-sm:text-[0.6rem]">
+          Job Posts
+        </Link>
+        <Button asChild size="menu">
+          <Link to="/admin/job/create" className="max-sm:text-[0.6rem]">
+            Post A Job
+          </Link>
         </Button>
 
         <ModeToggle />
