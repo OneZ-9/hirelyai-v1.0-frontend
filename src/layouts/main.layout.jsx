a4ab1048aa2fee46afdd297c437b6537eba1990a
Chamod Tharuka <chamodtharuka98@gmail.com>
2024-09-01 19:00:15 +0530

    Changes the layout containers for main and root layouts

--- src/layouts/main.layout.jsx
+++ src/layouts/main.layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <div>
+    <div className="flex flex-col">
       <Navigation />
 
       <Outlet />
--- src/layouts/root.layout.jsx
+++ src/layouts/root.layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function RootLayout() {
   return (
-    <main>
+    <main className="max-w-[1400px] mx-auto p-4">
       <Outlet />
     </main>
   );
